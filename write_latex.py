833833401cc8268c7150d69de8a006146737949d
Konstantin Emil Thiel <konstantin.thiel@pmu.ac.at>
2022-05-25 16:23:02 +0200

    Add power table pylatex skeleton

--- write_latex.py
+++ write_latex.py
@@ -1,33 +1,71 @@
-from pylatex import Document, LongTable, MultiColumn
+from pylatex import Document, LongTable, MultiColumn, Tabular
+from pylatex.utils import NoEscape, bold
+from typing import Iterable
+from pandas import DataFrame
 
-def genenerate_combined_power_table(
-    outfiles: Iterable[str],
-    period: str) -> None:
 
-    geometry_options = {
-        "margin": "2.54cm",
-        "includeheadfoot": True
-    }
-    doc = Document(page_numbers=True, geometry_options=geometry_options)
+def fill_power_table_segment(
+    table: Tabular,
+    df: DataFrame) -> None:
+    
 
-    # Generate data table
-    with doc.create(LongTable("l l l")) as data_table:
-            data_table.add_hline()
-            data_table.add_row(["header 1", "header 2", "header 3"])
-            data_table.add_hline()
-            data_table.end_table_header()
-            data_table.add_hline()
-            data_table.add_row((MultiColumn(3, align='r',
-                                data='Continued on Next Page'),))
-            data_table.add_hline()
-            data_table.end_table_footer()
-            data_table.add_hline()
-            data_table.add_row((MultiColumn(3, align='r',
-                                data='Not Continued on Next Page'),))
-            data_table.add_hline()
-            data_table.end_table_last_footer()
-            row = ["Content1", "9", "Longer String"]
-            for i in range(150):
-                data_table.add_row(row)
 
-    doc.generate_pdf("longtable", clean_tex=False)
+table = Tabular("|cclcl|")
+table.add_row(
+    (MultiColumn(1),
+    MultiColumn(4, data=bold("Power")))
+)
+table.append(NoEscape(r"\cline{2-5}"))
+table.add_row(
+    (MultiColumn(1),
+    MultiColumn(2, align="|c|", data=bold("Pruritus")),
+    MultiColumn(2, align="c|", data=bold("Pain")))
+)
+table.append(NoEscape(r"\cline{2-5}"))
+table.add_row(
+    (MultiColumn(1),
+    MultiColumn(1, align="|c|", data="Scenario 1"),
+    MultiColumn(1, align="c|", data="Scenario 2"),
+    MultiColumn(1, align="c|", data="Scenario 1"),
+    MultiColumn(1, align="c|", data="Scenario 2"))
+)
+table.append(NoEscape(r"\cline{2-5}"))
+table.add_row(
+    (MultiColumn(1),
+    MultiColumn(4, align="|c|", data=bold("nparLD")))
+)
+table.add_hline()
+table.add_hline()
+table.add_row(
+    (MultiColumn(1, align="|c|", data=NoEscape(r"$\mu_{\mbox{\scriptsize log}}=0.2$")),
+    MultiColumn(1, align="|c|", data=0.1948),
+    MultiColumn(1, align="|c|", data=0.1896),
+    MultiColumn(1, align="|c|", data=0.2340),
+    MultiColumn(1, align="|c|", data=0.2226))
+)
+table.add_hline()
+table.add_row(
+    (MultiColumn(1, align="|c|", data=NoEscape(r"$\mu_{\mbox{\scriptsize log}}=0.6$")),
+    MultiColumn(1, align="|c|", data=0.3160),
+    MultiColumn(1, align="|c|", data=0.2906),
+    MultiColumn(1, align="|c|", data=0.3242),
+    MultiColumn(1, align="|c|", data=0.3018))
+)
+table.add_hline()
+table.add_row(
+    (MultiColumn(1, align="|c|", data=NoEscape(r"$\mu_{\mbox{\scriptsize log}}=0.9$")),
+    MultiColumn(1, align="|c|", data=0.4372),
+    MultiColumn(1, align="|c|", data=0.3980),
+    MultiColumn(1, align="|c|", data=0.4230),
+    MultiColumn(1, align="|c|", data=0.3802))
+)
+table.add_hline()
+table.add_hline()
+
+doc = Document()
+doc.append(table)
+doc.generate_pdf("testtable", clean_tex=False)
+
+
+
+
